bde89d8665aeec7d618eda678d8dfe72145a5c32
om <om@om-PC>
2018-09-29 14:56:27 +0530

    Java git test class

---
+++ GitTest/src/com/practice/HelloGit.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = { "Bilbo", "Frodo", "Aragon", "Legolas", "Gundalf" };
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("Hello names" + names);
+		}
+	}
+
+}
